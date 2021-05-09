a719cb342cc14bf8897273d7d1df521a7859f0c4
JulitaD <vilkejd@gmail.com>
2021-05-09 20:01:31 +0300

    Added Base Page

---
+++ Page/BasePage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaigiamasisDarbas2021
+{
+    public class BasePage
+    {
+        protected static IWebDriver Driver;
+        public BasePage(IWebDriver webDriver)
+        {
+            Driver = webDriver;
+        }
+
+        public void CloseBrowser()
+        {
+            Driver.Close();
+        }
+
+        public WebDriverWait GetWait(int seconds = 10)
+        {
+            return new WebDriverWait(Driver, TimeSpan.FromSeconds(seconds));
+        }
+    }
+}
